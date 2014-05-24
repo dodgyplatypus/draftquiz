e9b766d5beafcf4b1bab349bd41f5e2629e22935
JTS-FIN <jts@kapsi.fi>
2014-05-24 23:14:51 +0300

    Match class (data-object), updateMatch.php to make use of it

---
+++ inc/class/Match.php
@@ -0,0 +1,53 @@
+<?php
+require_once('Error.php');
+require_once('config.php');
+
+/**
+ * Presents a single Dota 2 match
+ */
+class Match {
+	public $id;
+	public $startTime;
+	public $duration;
+	public $winner;
+	public $mode;
+	
+	public function __construct() {
+		$args = func_get_args();
+		switch (func_num_args()):
+			case 1:
+				$this->id = (int) $args[0];
+				break;
+		endswitch;
+	}
+	
+	public function fetchFromApi() {
+		if (!isset($this->id)) {
+			Error::outputError('Can\'t fetch from API: No ID given', 'Match->fetchFromApi');
+			return false;
+		}
+		if ($json = file_get_contents('https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?match_id=' . $this->id . '&key=' . API_KEY)) {
+			$db = PdoFactory::getInstance(DB_CONNECTION, DB_USER, DB_PW);
+			$matchData = json_decode($json, true);
+			$this->startTime = $matchData['result']['start_time'];
+			$this->duration = $matchData['result']['duration'];
+			$this->winner = $matchData['result']['radiant_win'] == true ? 1 : 0;
+			$this->mode = $matchData['result']['game_mode'];
+		}
+	}
+	
+	public function saveToDb() {
+		$db = PdoFactory::getInstance(DB_CONNECTION, DB_USER, DB_PW);
+		$db->beginTransaction();
+		$sql = 'INSERT INTO ' . DB_TABLE_PREFIX . 'match SET id = :id, start_time = :start_time, duration = :duration, winner = :winner, mode = :mode ON DUPLICATE KEY UPDATE start_time = :start_time, duration = :duration, winner = :winner, mode = :mode';
+		try {
+			$stmt = $db->prepare($sql);
+			$stmt->execute(array(':id' => $this->id, ':start_time' => $this->startTime, ':duration' => $this->duration, ':winner' => $this->winner, ':mode' => $this->mode));
+			$db->commit();
+		}
+		catch(PDOException $e) {
+			$db->rollBack();
+			Error::outputError("Failed to insert match data to database", $e->getMessage(), 1);
+		}
+	}
+}
