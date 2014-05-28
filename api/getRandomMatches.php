68bd8ed40a3ddea502d7157eb3a5ed73161d2ac5
JTS-FIN <jarkko@lauspalo.com>
2014-05-28 23:49:37 +0300

    To only give needed data

--- api/getRandomMatches.php
+++ api/getRandomMatches.php
@@ -7,8 +7,22 @@ require_once(INC_PATH . 'class/PdoFactory.php');
 require_once(INC_PATH . 'class/Match.php');
 require_once(INC_PATH . 'class/MatchManager.php');
 
+$count = isset($_GET['count']) ? (int) $_GET['count'] : 10;
+
 $matchManager = new MatchManager();
-$matches = $matchManager->getRandomMatches(10);
+$matches = $matchManager->getRandomMatches($count);
+
+$output = array();
+if (is_array($matches)) {
+	foreach ($matches AS $m) {
+		$players = array();
+		foreach ($m->players AS $p) {
+			$players[] = array('hero' => $p['hero_id'], 'team' => $p['team'], 'position' => $p['position']);
+			
+		}
+		$output[] = array("publicId" => $m->publicId, "players" => $players);
+	}
+}
 
 header('Content-Type: application/json');
-echo json_encode($matches);
+echo json_encode($output);
