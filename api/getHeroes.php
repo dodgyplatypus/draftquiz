c7eba7135291259990ece70a51afbf9a038c7e0f
JTS-FIN <jarkko@lauspalo.com>
2014-05-29 21:34:48 +0300

    Attribute added

--- api/getHeroes.php
+++ api/getHeroes.php
@@ -7,7 +7,7 @@ require_once(INC_PATH . 'class/PdoFactory.php');
 
 $db = PdoFactory::getInstance(DB_CONNECTION, DB_USER, DB_PW);
 
-$stmt = $db->prepare('SELECT id, name, en_name FROM ' . DB_TABLE_PREFIX . 'hero');
+$stmt = $db->prepare('SELECT id, name, en_name, attr FROM ' . DB_TABLE_PREFIX . 'hero');
 $stmt->execute();
 
 $output = array();
