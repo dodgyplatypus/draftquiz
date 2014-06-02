6a8dfd5d16e00b7929cb26aa74aaf7f45fef3129
JTS-FIN <jarkko@lauspalo.com>
2014-06-02 19:18:13 +0300

    Doesn´t insert anything to DB, Match-objects does that now

--- inc/class/MatchManager.php
+++ inc/class/MatchManager.php
@@ -17,59 +17,32 @@ class MatchManager {
 		
 		$db = PdoFactory::getInstance(DB_CONNECTION, DB_USER, DB_PW);
 		$skill = 3;
-		
 		// get new match data
 		$json = file_get_contents('https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/V001/?min_players=10&skill=' . $skill . '&matches_requested=' . $count . '&key=' . API_KEY);
 
 		$matches = json_decode($json, true);
-		
 		$matchList = array();
 		// execute queries
 		for($i = 0; $i < count($matches['result']['matches']); $i++) {
 			$match = $matches['result']['matches'][$i];
-			if($this->isValidMatch($match)) {				
+			if($this->isValidMatch($match)) {
 				try {
 					// test if match is already on the database, if it is, just go to the next one
 					$testMatch = new Match($match['match_id']);
-					if ($match->loadFromDb !== false) {
+					if ($testMatch->loadFromDb() !== false) {
 						continue;
-					}
-					$db->beginTransaction();
-					// insert match
-					$sql = 'INSERT INTO `' . DB_TABLE_PREFIX . 'match` (`match_id`, `start_time`) VALUES (:id, :start_time)';
-					$stmt = $db->prepare($sql);
-					$stmt->bindValue(':id', $match['match_id'], PDO::PARAM_INT);
-					$stmt->bindValue(':start_time', $match['start_time'], PDO::PARAM_INT);
-					$stmt->execute();
-
-					// insert players
-					for($j = 0; $j < 10; $j++) {
-						$player = $match['players'][$j];
-						$sql = 'INSERT INTO `' . DB_TABLE_PREFIX . 'match_player` (`account_id`, `match_id`, `hero_id`, `position`) VALUES (:account_id, :match_id, :hero_id, :position)';
-						$stmt = $db->prepare($sql);
-						$stmt->bindValue(':account_id', $player['account_id'], PDO::PARAM_INT);
-						$stmt->bindValue(':match_id', $match['match_id'], PDO::PARAM_INT);
-						$stmt->bindValue(':hero_id', $player['hero_id'], PDO::PARAM_INT);
-						$stmt->bindValue(':position', $player['player_slot'], PDO::PARAM_INT);
-						$stmt->execute();
-					}
-					
+					}					
 					// get detailed match data
 					$matchObject = new Match($match['match_id']);
 					$matchObject->fetchFromApi();
 					if ($matchObject->isValid() === true) {
-						$db->commit();
 						$matchObject->skill = $skill;
 						$matchObject->saveToDb();
 						$matchList[] = $matchObject;
 					}
-					else {
-						$db->rollBack();
-					}
 				}
 				catch (PDOException $e) {
-					Error::outputError('Failed to insert match/players data', $e->getMessage(), 1);
-					$db->rollBack();
+					Error::outputError('Failed to insert match/players data' . $e->getMessage(), $e->getMessage(), 1);
 				}
 			}
 		}
