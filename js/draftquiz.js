e4c74f40e350f6b14cb5ac6c787e0b0a7ad1a5d5
JTS-FIN <jarkko@lauspalo.com>
2014-06-09 23:26:15 +0300

    Nextmatch, init to document.ready since we cant be sure about dom being loaded

--- js/draftquiz.js
+++ js/draftquiz.js
@@ -1,4 +1,6 @@
-MatchManager.init();
+$(document).ready(function() {
+	MatchManager.init();
+});
 
 $(document).on('click', '.vote-dire', function() {
 	MatchManager.guessWinner(0);
@@ -7,5 +9,10 @@ $(document).on('click', '.vote-dire', function() {
 
 $(document).on('click', '.vote-radiant', function() {
 	MatchManager.guessWinner(1);
+	return false;
+});
+
+$(document).on('click', '#button-nextmatch', function() {
+	MatchManager.nextMatch();
 	return false;
 });
