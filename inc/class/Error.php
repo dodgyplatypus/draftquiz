4f59f5fb074b7d952319d4beee078a9aedb34440
JTS-FIN <jarkko@lauspalo.com>
2014-06-12 23:43:10 +0300

    Now takes whole exception as argument, and uses stack trace

--- inc/class/Error.php
+++ inc/class/Error.php
@@ -3,14 +3,12 @@
 class Error {
 
 	// first argument is outputted to user, second is put to PHP error handler
-	public static function outputError($userOutput, $logOutput = false, $fatal = 0) {
+	public static function outputError($userOutput, $exception = false, $fatal = 0) {
 		print "Error: " . $userOutput;
-		if ($logOutput !== false) {
-			if ($fatal > 0) { $fatal = E_USER_ERROR; } 
-			else { $fatal = E_USER_NOTICE; }
-			
-			trigger_error($userOutput . ", details: " . $logOutput, $fatal);
-		}
+		if ($fatal > 0) { $fatal = E_USER_ERROR; } 
+		else { $fatal = E_USER_NOTICE; }
+		
+		trigger_error($userOutput . ", details: " . $exception->getMessage() . ", trace: " . print_r($exception->getTrace(), true), $fatal);
 	}
 
 }
